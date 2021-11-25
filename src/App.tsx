b52bdefe686dc76dc2c0b3ff4ec60ac24f88f620
ahuaynap <huaynalexis@gmail.com>
2021-11-25 18:01:16 -0500

    feat: Funcionalidad 100%

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,121 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
-interface Foo{
-  weight: number,
-  exercise: number,
-  calories_essential: number,
-  goal_type: string,
-  goal_value: number,
-  protein: number,
-  fats: number,
-  carbos: number,
-  goal_calories: number
-}
 
 const App = () => {
 
-  //const
+  const [weight, setWeight] = useState(0);
+  const [exerciseFrecuency, setExerciseFrecuency] = useState(0);
+  const [essentialCalories, setEssentialCalories] = useState(0);
+  const [goal, setGoal] = useState('');
+  const [maxPercentage, setMaxPercentage] = useState(0);
+  const [goalPercentage, setGoalPercentage] = useState(0);
+  const [goalCalories, setGoalCalories] = useState(0);
+  const [totalCalories, setTotalCalories] = useState(0);
+  const [proteins, setProteins] = useState(0);
+  const [totalProteins, setTotalProteins] = useState(0);
+  const [fats, setFats] = useState(0);
+  const [totalFats, setTotalFats] = useState(0);
+  const [carbohydrates, setCarbohydrates] = useState(0);
+
+  useEffect(() => {
+    setEssentialCalories(weight * exerciseFrecuency * 22);
+    setGoalCalories(essentialCalories * goalPercentage / 100.0);
+    setTotalCalories(goal == 'U' ? essentialCalories + goalCalories : essentialCalories - goalCalories);
+    setTotalProteins(proteins * weight);
+    setTotalFats(fats * weight);
+    setCarbohydrates((totalCalories - (totalProteins * 4 + totalFats * 9))/4);
+  });
+
+  const objetivoU = () => {
+    setGoal('U');
+    setMaxPercentage(20);
+  }
+
+  const objetivoL = () => {
+    setGoal('L');
+    setMaxPercentage(30);
+  }
+
+  const goalUpdater = (e:any) => {
+    setGoalPercentage(parseInt(e.target.value))
+  }
 
   return (
-    <div className="bg-purple-100">
+    <div className="">
       <h1>Calc diet</h1>
-      <p>weight (kg)</p>
-      <input type="range" min="10" max="150" step="0.1"/>
-      <p>diary activity</p>
-      <input type="range" min="1.2" max="2.0" step="0.2"/>
+      <p>weight {weight.toFixed(1)} (kg)</p>
+      <button
+        className="w-5  bg-red-600"
+        onClick={() => {setWeight(weight - 0.1)}}
+        >
+        -
+      </button>
+      <input
+        type="range"
+        min="30" max="130" step="0.1"
+        value={weight}
+        onChange={(e) => {
+          setWeight(parseFloat(e.target.value));
+        }}
+      />
+      <button
+        className="w-5  bg-red-600"
+        onClick={() => {setWeight(weight + 0.1)}}
+        >
+        +
+      </button>
+      <p>diary activity {exerciseFrecuency}</p>
+      <input
+        type="range"
+        min="1.2" max="2.2" step="0.1"
+        value={exerciseFrecuency}
+        onChange={(e) => {
+          setExerciseFrecuency(parseFloat(e.target.value));
+        }}
+      />
       <p>Formula</p>
-      <h3>Resultado de formula</h3>
+      <p>Calorias Necesarias : {weight.toFixed(2)} X {exerciseFrecuency} X 22 = {essentialCalories.toFixed(2)} kcal</p>
       <p>Objetivo</p>
-      <input type="radio" name="" id="" value=""/>
-      <label htmlFor="">Subir de peso</label>
-      <input type="radio" name="" id="" value=""/>
-      <label htmlFor="">bajar de peso</label>
-      <p>Rango segun el objetivo</p>
-      <input type="range" min="10" max="30" step="1"/>
-      <p>Se muestra el valor de las calorias obejitvos</p>
-      <p>Se muestra la formula</p>
-      <p>Calculo de macronutrientes</p>
+      <label htmlFor="">
+        <input type="radio" name={goal} value="up" onChange={objetivoU}/>
+        Subir de peso
+      </label>
+      <label htmlFor="">
+        <input type="radio" name={goal} value="low" onChange={objetivoL}/>
+        bajar de peso
+      </label>
+      <p>Rango segun el objetivo {goal == 'U' ? "+" : "-"}  {goalPercentage}%</p>
+      <input
+        type="range"
+        min="10" max={maxPercentage} value={goalPercentage}
+        onChange={e => goalUpdater(e)}
+        step="1"/>
+      <p>Se muestra el valor de las calorias objetivos {goalCalories.toFixed(2)} kcal</p>
+      <p>total Calories : {essentialCalories.toFixed(2)} {goal == 'U' ? "+" : "-"} {goalCalories.toFixed(2)} = {totalCalories.toFixed(2)}</p>
+      <h3>Calculo de macronutrientes</h3>
       <p>Hint de los valores</p>
-      <p>Proteinas</p>
-      <input type="range" min="1.8" max="2.5" step="0.1" />
-      <p>Resultado</p>
-      <p>Grasas</p>
-      <input type="range" min="0.5" max="1.5" step="0.1" />
-      <p>Resultado</p>
-      <p>Carbohidratos</p>
-      <p>Resultado</p>
+      <p>Proteinas {proteins} g por kilogramo de peso</p>
+      <input
+        type="range"
+        min="1.5" max="2.5" step="0.1"
+        value={proteins}
+        onChange={(e) => {
+          setProteins(parseFloat(e.target.value));
+        }}/>
+      <p>Proteinas totales: {totalProteins.toFixed(2)} g  = {(totalProteins * 4.0).toFixed(2)} kcal</p>
+      <p>Grasas {fats} g por kilogramo de peso</p>
+      <input
+        type="range"
+        min="0.3" max="1.5" step="0.1"
+        value={fats}
+        onChange={(e) => {
+          setFats(parseFloat(e.target.value));
+        }}
+        />
+      <p>Grasas totales: {totalFats.toFixed(2)} g = {(totalFats * 9).toFixed(2)} kcal</p>
+      <p>Carbohidratos es el resto</p>
+      <p>Carbohidratos totales: {carbohydrates.toFixed(2)} g = {(carbohydrates * 4.0).toFixed(2)} kcal</p>
+      <p>{(totalProteins * 4.0).toFixed(2)} + {(totalFats * 9).toFixed(2)} + {(carbohydrates * 4).toFixed(2)} = {totalCalories.toFixed(2)}</p>
     </div>
   );
 }
